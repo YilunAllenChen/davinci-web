2b274397a18f336fd8a2c6e164f8ab8afcd475c9
YilunAllenChen <allenchenyilun1999@gmail.com>
2026-03-03 20:26:43 -0600

    Updates

--- src/firebaseModules.js
+++
@@ -1,4 +0,0 @@
-export default function log(message, data = {}) {
-  // Firebase logging was removed; keep call sites working for static builds.
-  console.log("[telemetry]", message, data);
-}
--- src/states/action/actionNavigation.jsx
+++
@@ -1,5 +0,0 @@
-export function navigateToAction(state, action) {
-  let { to } = action.payload;
-  state.navigation.curr = to;
-  console.log("user now navigating %s", to);
-}
